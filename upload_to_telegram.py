778ed6063db977c0f8d901713790de64cf7c1d63
Ahnaf Tauseef <158799743+AhnafTouseef@users.noreply.github.com>
2026-09-18 23:29:06 +0600

    Use subprocess to install required packages
    Replaced pip install command with subprocess.run for package installation.

--- upload_to_telegram.py
+++ upload_to_telegram.py
@@ -1,7 +1,7 @@
-# 1. Install required packages
-!pip install telethon hachoir nest_asyncio
-
 import requests
+import subprocess
+
+subprocess.run(["pip", "install", "telethon", "hachoir," "nest_asyncio"])
 
 # Fetch the raw text of whatever script you need
 script_url = "https://raw.githubusercontent.com/AhnafTouseef/Colab-Tools/main/colab_file_naviagator.py"
